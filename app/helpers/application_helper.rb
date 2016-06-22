00b5e4977df7d07fa752f8240f0deddccf170248
Igor <siafin2010@gmail.com>
2016-06-22 18:20:57 +0300

    need to do helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def glyph_link_to(glyph, text, *args, &block)
+  	link_to(*args, &block) do
+  		capture do	
+  		concat content_tag("span",)
+
+
+  		end
+  	end
+  end
 end
